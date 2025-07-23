92665b095f81af4b8b3c3cbffa60e080d6041061
Cyphmuk <darshankm2025@gmail.com>
2025-07-23 14:40:08 +0530

    Fix: clean static index.html path

--- main.py
+++ main.py
@@ -54,4 +54,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 def read_root():
-    return FileResponse(os.path.join("static", "index.html"))
+return
+FileResponse(os.path.join("static","index.html"))
